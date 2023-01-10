af94cf110defe4b87fc92fc233134322a55a4dd7
natamartins <nata.codedev@gmail.com>
2023-01-10 17:59:44 -0300

    commit init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import GlobalStyles from "../src/styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="" />
+        <link rel="apple-touch-icon" href="" />
+        <meta
+          name="decription"
+          content="project react, nextjs and styled-component"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
